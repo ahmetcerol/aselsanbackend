23bab3f5e98355f733576216dff7da609a6282e7
AhmetCanErol <erola2917@gmail.com>
2023-08-23 11:35:49 +0300

    23.08.23/2

--- src/main/java/com/aselsanbackend/AselsanBackend/security/TcKimlikNoVerification.java
+++ src/main/java/com/aselsanbackend/AselsanBackend/security/TcKimlikNoVerification.java
@@ -11,9 +11,7 @@ public class TcKimlikNoVerification {
     }
 
     private boolean isValidFormat(String tcKimlikNo) {
-        String regex = "^[1" +
-                "" +
-                "-9][0-9]{10}$";
+        String regex = "^[1-9][0-9]{10}$";
         return tcKimlikNo.matches(regex);
     }
 
